44ec1083b72d26401aa532679f3dfb1113d12ad0
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-02-10 01:51:07 -0200

    add scrapy project

---
+++ searchlinks/pipelines.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymongo
+import sqlite3
+
+
+class SearchlinksPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+
+class MongoPipeline(object):
+
+    collection_name = 'duckduckgo'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
+        return item
+
+
+class SQLiteStorePipeline(object):
+    filename = '/home/leonardo/Documents/searchlinks/searchlinks'
+
+    def process_item(self, item, spider):
+        try:
+            self.conn.execute('insert into duckduckgo values(?,?,?)',
+                              (item['cnpj'], item['url'], item['name'],))
+            self.conn.execute('update inputsc set status=200 where cnpj=?',
+                              (item['cnpj'],))
+            self.conn.commit()
+        except sqlite3.Error as e:
+            spider.logger.info('Failed to insert item {}'.format(e.args[0]))
+        return item
+
+    def open_spider(self, spider):
+        try:
+            self.conn = sqlite3.connect(self.filename)
+        except:
+            spider.logger.info('error connect to sqlite')
+
+    def finalize(self):
+        if self.conn is not None:
+            self.conn.commit()
+            self.conn.close()
+            self.conn = None
